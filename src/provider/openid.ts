f02bf299e9124f05ad8388715f79c9ce9dd30bfe
rhangai <hangai.renan@gmail.com>
2020-06-18 21:56:37 -0300

    chore: Updated documentation

--- src/provider/openid.ts
+++ src/provider/openid.ts
@@ -11,7 +11,6 @@ export type ProviderOpenIdConfig = {
 	providerClientSecret: string;
 	providerAuthUrl: string;
 	providerTokenUrl: string;
-	providerValidateUrl: string;
 	providerUserinfoUrl: string;
 	providerRedirectUrl: string;
 	providerJwksUrl?: string;
