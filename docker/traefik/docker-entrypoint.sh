a33c126fd94ac6058853fbb614c700fa5ab79512
rhangai <hangai.renan@gmail.com>
2020-07-09 20:26:40 -0300

    chore: Updated traefik container

--- docker/traefik/docker-entrypoint.sh
+++ docker/traefik/docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "$TRAEFIK_DEBUG" = "1" ]; then
+	cp /etc/traefik/traefik.debug.toml /etc/traefik/traefik.toml
+else
+	cp /etc/traefik/traefik.prod.toml /etc/traefik/traefik.toml
+fi
 if [ -n "$TRAEFIK_CONFIG" ]; then
 	echo "$TRAEFIK_CONFIG" > /etc/traefik/providers/default.toml
 fi
