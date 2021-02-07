8e7c71f477f146d900f5f8c13693f3f71f7a57a7
rhangai <hangai.renan@gmail.com>
2021-02-07 20:08:35 -0300

    chore: Updated build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,8 @@ SCRIPT_FILE=$(readlink -f $0)
 SCRIPT_DIR=$(dirname "$SCRIPT_FILE")
 ROOT_DIR=$(dirname "$SCRIPT_DIR")
 
-
 cd "$ROOT_DIR"
-VERSION=$(cargo metadata --no-deps --format-version 1 | jq -r ".packages[0].version")
+VERSION=$(cat package.json | jq -r ".version")
 docker build . -f docker/bin.dockerfile -t "rhangai/auth-gatekeeper:bin" -t "rhangai/auth-gatekeeper:bin-${VERSION}"
 docker build . -f docker/dockerfile -t "rhangai/auth-gatekeeper" -t "rhangai/auth-gatekeeper:${VERSION}"
 
