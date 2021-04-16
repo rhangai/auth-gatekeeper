379dc7ced125fd39843cf4446659575be86e2dbb
rhangai <hangai.renan@gmail.com>
2021-04-16 11:20:10 -0300

    chore: Improved build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,11 +6,16 @@ ROOT_DIR=$(dirname "$SCRIPT_DIR")
 
 cd "$ROOT_DIR"
 VERSION=$(cat package.json | jq -r ".version")
-docker build . -f docker/bin.dockerfile -t "rhangai/auth-gatekeeper:bin" -t "rhangai/auth-gatekeeper:bin-${VERSION}"
-docker build . -f docker/dockerfile -t "rhangai/auth-gatekeeper" -t "rhangai/auth-gatekeeper:${VERSION}"
+VERSION_MAJOR=$(echo "$VERSION" | cut -d '.' -f 1)
+VERSION_MINOR=$(echo "$VERSION" | cut -d '.' -f 2)
+VERSION_SHORT="${VERSION_MAJOR}.${VERSION_MINOR}"
+docker build . -f docker/bin.dockerfile -t "rhangai/auth-gatekeeper:bin" -t "rhangai/auth-gatekeeper:bin-${VERSION_SHORT}" -t "rhangai/auth-gatekeeper:bin-${VERSION_MAJOR}"
+docker build . -f docker/dockerfile -t "rhangai/auth-gatekeeper" -t "rhangai/auth-gatekeeper:${VERSION_SHORT}" -t "rhangai/auth-gatekeeper:${VERSION_MAJOR}"
 
 echo "Built tags"
 echo "  rhangai/auth-gatekeeper:bin"
-echo "  rhangai/auth-gatekeeper:bin-${VERSION}"
+echo "  rhangai/auth-gatekeeper:bin-${VERSION_MAJOR}"
+echo "  rhangai/auth-gatekeeper:bin-${VERSION_SHORT}"
 echo "  rhangai/auth-gatekeeper"
-echo "  rhangai/auth-gatekeeper:${VERSION}"
+echo "  rhangai/auth-gatekeeper:${VERSION_MAJOR}"
+echo "  rhangai/auth-gatekeeper:${VERSION_SHORT}"
