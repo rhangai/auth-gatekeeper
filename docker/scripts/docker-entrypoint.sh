0f61bb6cf9e5d75ca8f8b31ee3b0675f80911c65
rhangai <hangai.renan@gmail.com>
2021-07-21 17:42:09 +0000

    chore: Updated cors implementation

--- docker/scripts/docker-entrypoint.sh
+++ docker/scripts/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir -p /etc/traefik/providers
 if [ "$TRAEFIK_DEBUG" = "1" ]; then
 	cp /etc/traefik/traefik.debug.toml /etc/traefik/traefik.toml
 else
@@ -8,4 +9,5 @@ fi
 if [ -n "$TRAEFIK_CONFIG" ]; then
 	echo "$TRAEFIK_CONFIG" > /etc/traefik/providers/default.toml
 fi
+dockerize -template  /etc/traefik/template/auth.toml:/etc/traefik/providers/auth.toml
 exec "$@"
